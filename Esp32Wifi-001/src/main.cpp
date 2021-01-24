f09f1b71b03f470b6865c29a27aab17e1a924158
walbertcm <36679434+walbertcm@users.noreply.github.com>
2021-01-24 12:40:24 -0300

    v1

---
+++ Esp32Wifi-001/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+
+const char* ssid="PROFESSOR_XLS";
+const char* password="qazwsxedc123";
+
+void setup() {
+  Serial.begin(115200);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
